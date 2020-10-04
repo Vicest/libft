4a93fa0a2bd7e981321c7a8dd702ccb0aadac143
Victor Martinez estrada <vicmarti@c1r13s6.42madrid.com>
2020-10-04 14:33:07 +0200

    Fixed compile errors.

--- ft_numdgts.c
+++ ft_numdgts.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 01:36:59 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/10/04 13:14:57 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/10/04 14:31:17 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@
 **	Number of digits in an integer, base 10.
 */
 
-size_t	ft_numdgts(unsigned long long n)
+size_t	ft_numdgts(long long n)
 {
 	size_t i;
+	unsigned long long	un;
 
 	i = 1;
-	while ((n /= 10) != 0)
+	un = n;
+	if(n < 0)
+		un = -1 * (n - 1) + 1;
+	while ((un /= 10) != 0)
 		i++;
 	return (i);
 }
--- ft_numdgts_base.c
+++ ft_numdgts_base.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 14:02:35 by vicmarti          #+#    #+#             */
-/*   Updated: 2020/10/04 13:13:33 by vicmarti         ###   ########.fr       */
+/*   Updated: 2020/10/04 14:31:17 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@
 **	Number of digits in an integer of a given base.
 */
 
-size_t	ft_numdgts_base(unsigned long long n, size_t baselen)
+size_t	ft_numdgts_base(long long n, size_t baselen)
 {
 	size_t i;
+	unsigned long long	un;
 
 	i = 1;
-	while ((n /= baselen) != 0)
+	un = n;
+	if(n < 0)
+		un = -1 * (n - 1) + 1;
+	while ((un /= baselen) != 0)
 		i++;
 	return (i);
 }
