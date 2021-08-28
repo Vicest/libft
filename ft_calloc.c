aa84809ef7de0084e0ad2e7d9aacae2f082ce764
Victor Martinez estrada <vicmarti@c1r7s2.42madrid.com>
2021-08-28 18:33:45 +0200

    Using a const to only mulltiply once.

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vicmarti <vicmarti@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 18:47:07 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/03/21 16:37:55 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/08/28 18:33:26 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*out;
+	const size_t	mem_bytes = count * size;
+	void			*out;
 
-	out = malloc(count * size);
+	out = malloc(mem_bytes);
 	if (out != NULL)
-		ft_bzero(out, count * size);
+		ft_bzero(out, mem_bytes);
 	return (out);
 }
