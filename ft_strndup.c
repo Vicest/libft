a1ba70f2fb120ff787c88eb05b8ba399b2b255a2
Victor Martinez estrada <vicmarti@c1r8s5.42madrid.com>
2021-08-30 20:13:23 +0200

    Off-By-One

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/19 11:17:45 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/08/30 20:00:13 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/08/30 20:12:52 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strndup(const char *s1, size_t n)
 	i = 0;
 	while (s1[i] && n > i)
 		i++;
-	out = ft_calloc(i + 1, 1);
+	out = ft_calloc(i, sizeof(char));
 	if (!out)
 		return (NULL);
 	while (i > 0)
