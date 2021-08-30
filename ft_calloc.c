9519ecee1ed94f7e496cbb493fbe3fc5c0a90aa2
Victor Martinez estrada <vicmarti@c1r8s5.42madrid.com>
2021-08-30 19:20:56 +0200

    Off-By-One in bzero. Calloc won't malloc if asked for 0.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <vicmarti@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 18:47:07 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/08/28 18:33:26 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/08/30 19:20:04 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 	const size_t	mem_bytes = count * size;
 	void			*out;
 
+	if (mem_bytes == 0)
+		return (NULL);
 	out = malloc(mem_bytes);
 	if (out != NULL)
 		ft_bzero(out, mem_bytes);
